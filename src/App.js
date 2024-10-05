68668f8490bdca56ebe7b241654e2d373810283d
Aaron <aaronmakaruk@proton.me>
2024-10-04 19:20:19 -0600

    Text change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, GitHub Pages!</h1>
+      <h1>Hello, HopeHub Pages!</h1>
       <p>This is a simple React app deployed on GitHub Pages.</p>
     </div>
   );
