c1f44c04ca5f9a3f4ac6c71357d8e7169cc639a7
Aaron <aaronmakaruk@proton.me>
2024-10-04 19:42:58 -0600

    firebase test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './firebaseConfig';
 
 function App() {
+  const [texts, setTexts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "texts"));
+      const textsArray = querySnapshot.docs.map(doc => doc.data().text);
+      setTexts(textsArray);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello, HopeHub Pages!</h1>
-      <p>This is a simple React app deployed on GitHub Pages.</p>
+      <h1>List of Texts</h1>
+      <ul>
+        {texts.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPeHtjHKNLH9J8iLSDQMcpvGO-XvwtqS0",
+  authDomain: "my-simple-react-app-8b29b.firebaseapp.com",
+  projectId: "my-simple-react-app-8b29b",
+  storageBucket: "my-simple-react-app-8b29b.appspot.com",
+  messagingSenderId: "318914215094",
+  appId: "1:318914215094:web:05fb6e784ed1729c65fa17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
